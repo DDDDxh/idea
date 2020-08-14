e9031ebbe75ef51a278f35fca676b9379846ab40
DDDDxh <244012506@qq.com>
2020-08-14 19:59:56 +0800

    idea第二次

--- src/com/dxh/test/DemoTest.java
+++ src/com/dxh/test/DemoTest.java
@@ -5,5 +5,6 @@ public class DemoTest {
     public static void main(String[] args) {
         System.out.println("idea test----------------------------");
         System.out.println("添加第二次");
+        System.out.println("wahaaaa");
     }
 }
